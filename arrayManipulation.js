46b387e7998dfebeabc223065d0ada941d07f100
Arnold-Gyebi <arnoldgyebi22@gmail.com>
2024-05-19 21:28:21 +0000

    Task two done

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -17,3 +17,28 @@ function processArray(number){
 console.log(processArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 
+function formatArrayStrings(string, number) {
+    let stringArr = []
+
+    for (let i = 0; i < string.length; i++) {
+        let str = string[i]
+        let num = number[i]
+
+        if (num % 2 === 0) {
+            stringArr[stringArr.length] = str.toUpperCase()
+        } else {
+            stringArr[stringArr.length] = str.toLowerCase()
+        }
+        
+    }
+
+    return stringArr
+}
+// Test the code
+
+console.log(formatArrayStrings(["hi", "john", "where", "are", "you"], processArray([3, 2, 1, 4, 7])))
+
+module.exports = {
+    processArray,
+    formatArrayStrings
+};
